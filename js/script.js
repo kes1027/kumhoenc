93f3fd6a24b824a818dc559a4562f912cb6a5768
김은소 <k.eunso1027@gmail.com>
2024-05-28 16:58:18 +0900

    05.28_금호건설

--- js/script.js
+++ js/script.js
@@ -1,21 +1,36 @@
 // DOM 구조가 파악되면 실행
 $(function () {
-  // alert('준비완료!');
-
   // 대상을 변수에 저장
+  const $window = $(window);
   const $menu = $('.gnb > li');
   const $submenu = $('.submenu');
   const duration = 300;
+  const $header = $('#header');
 
   // 메뉴 영역에 마우스가 들어오면
   $menu.on('mouseenter', function () {
-    $(this).find($submenu).stop().slideDown(duration);
-    $(this).addClas('on');
+    $submenu.stop().show();
+    // 활성화된 메뉴 표시 : on 클래스 부여
+    $(this).addClass('on');
+    // header에 active 클래스 부여
+    $header.addClass('active');
   });
 
   // 메뉴 영역에 마우스가 나가면
   $menu.on('mouseleave', function () {
-    $submenu.stop().slideUp(duration);
-    $(this).removeClass('on');
+    $submenu.stop().hide();
+    $menu.removeClass('on');
+    $header.removeClass('active');
+  });
+
+  // 마우스 휠을 조작할 때
+  $window.on('wheel', function (e) {
+    if (e.originalEvent.wheelDelta > 0) {
+      // 휠을 올렸을 때
+      $header.removeClass('hide');
+    } else {
+      // 휠을 내렸을 때
+      $header.addClass('hide');
+    }
   });
 });
