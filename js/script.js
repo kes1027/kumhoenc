4bc29ff26043f70959f6d36cb418f6ee7f86912e
김은소 <k.eunso1027@gmail.com>
2024-07-02 17:31:35 +0900

    07.02

--- js/script.js
+++ js/script.js
@@ -59,7 +59,7 @@ $(function () {
 
   // 슬라이더
   // s: 3. 초기화 실행
-  const projectSlider = new Swiper('.visual-slider', {
+  const visualSlider = new Swiper('.visual-slider', {
     loop: true,
     slidesPerView: 'auto',
     // spaceBetween: 40,
@@ -85,16 +85,92 @@ $(function () {
   $('.btn_pause').click(function () {
     if (sw == 0) {
       $('.btn_pause').addClass('on');
-      projectSlider.autoplay.stop();
+      visualSlider.autoplay.stop();
       sw = 1;
     } else {
       $('.btn_pause').removeClass('on');
-      projectSlider.autoplay.start();
+      visualSlider.autoplay.start();
       sw = 0;
     }
     console.log(sw);
   });
   // 멈춤 버튼
 
+  // ---------------------------------- //
+
+  // 프로젝트 슬라이더
+  const projectSlider = new Swiper('.project-slider', {
+    loop: true,
+    slidesPerView: 'auto',
+    // spaceBetween: 40,
+
+    // autoplay: {
+    //   delay: 3000,
+    // },
+    speed: 1000, //슬라이드 되는 속도 : 기본값 300ms (0.3초)
+    effect: 'slide', // 기본값(slide), fade, cube, coverflow, flip, card 등
+
+    // Navigation arrows
+    navigation: {
+      prevEl: '.project-button-prev',
+      nextEl: '.project-button-next',
+    },
+  });
+  // 프로젝트 슬라이더
+
+  // ---------------------------------- //
+  // s: news
+  gsap.registerPlugin(ScrollTrigger);
+  const newsList1 = $('.news-list > li:first-child');
+  const newsList2 = $('.news-list > li:last-child');
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.news',
+      // markers: true,
+      start: 'top 0%',
+      end: 'bottom+=2000px',
+      pin: true, //트리거를 고정
+      // scrub: 1, //스크롤 값을 따라잡게, 1초 동안
+    },
+  });
+  // e: news
+  // ---------------------------------- //
+  tl.to(newsList1, {
+    yPercent: -90,
+    // duration: 30,
+    // ease: 'power4.in',
+    scrollTrigger: {
+      trigger: '.news',
+      // markers: true,
+      start: 'top 0%',
+      end: 'bottom+=2000px',
+      scrub: 1,
+    },
+  });
+  tl.to(
+    newsList2,
+    {
+      yPercent: -70,
+      // duration: 30,
+      // ease: 'power4.in',
+      scrollTrigger: {
+        trigger: '.news',
+        // markers: true,
+        start: 'top 0%',
+        end: 'bottom+=2000px',
+        scrub: 1,
+      },
+    },
+    '<'
+  );
+  // ---------------------------------- //
+  // family site
+  $('.family-site select').on('change', function () {
+    const linkValue = $(this).val();
+    window.open(linkValue);
+  });
+
+  // ---------------------------------- //
   // end:jquery
 });
